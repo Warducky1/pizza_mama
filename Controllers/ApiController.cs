e70287fb2a750ec78ef6f2f877ac28da7a1609c1
BoitardCédric <150232839+Warducky1@users.noreply.github.com>
2023-12-06 12:06:28 +0100

    Add files via upload

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pizza_mama.Data;
+using Pizza_mama.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Pizza_mama.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ApiController : Controller
+    {
+        public readonly DataContext _context;
+
+        public ApiController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        [Route("GetPizzas")]
+        public IActionResult GetPizzas()
+        {
+            //var pizza = new Pizza() { name = "PizzaTest", price = 10.5f, vegetarian= false, ingredients="Tomates, lardon, chedar" };
+
+            var pizzas = _context.Pizzas.ToList();
+
+            return Json(pizzas);
+        }
+    }
+}

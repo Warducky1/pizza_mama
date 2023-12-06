130d5fbd40a356c48c9e920a1f04b4385f8c019a
BoitardCédric <150232839+Warducky1@users.noreply.github.com>
2023-12-06 12:05:20 +0100

    Add files via upload

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pizza_mama.Models;
+
+namespace Pizza_mama.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Pizza> Pizzas { get; set; }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Pizza_mama.Models
+{
+    public class Pizza
+    {
+        [JsonIgnore]
+        public int PizzaID { get; set; }
+
+        [Display(Name = "Nom")]
+        public string name { get; set; }
+
+        [Display(Name = "Prix (€)")]
+        public float price { get; set; }
+        [Display(Name = "Végétarienne")]
+        public bool vegetarian { get; set; }
+
+        [JsonIgnore]
+        [Display(Name = "Ingrédient")]        
+        public string? ingredients { get; set; }
+
+        [NotMapped]
+        [JsonPropertyName("ingredients")]
+        public string[] listeIngredients 
+        { 
+            get
+            {
+                if(ingredients == null || ingredients.Count() == 0)
+                {
+                    return null;
+                }
+
+                return ingredients.Split(", ");
+            }
+        }
+
+    }
+}
